2a167ffadb6986409bd58d7401061b68b08879a3
Seung Wook Kim <CEO@Rloha.io>
2019-05-09 16:00:51 +0900

    Create day2_diamonds_data_handling.R

---
+++ day2_diamonds_data_handling.R
@@ -0,0 +1,29 @@
+data("diamonds", package = "ggplot2")
+df = diamonds # 복사(복제)
+head(df, 2)
+
+table(df$color)
+prop.table(table(df$color))
+tb_color = as.data.frame(prop.table(table(df$color)))
+tb_color
+
+colnames(tb_color)
+colnames(tb_color) = c("color", "prop")
+tb_color
+
+library("ggplot2")
+ggplot(data = tb_color, aes(x = color, y = prop)) + 
+  geom_col()
+
+# write.csv(tb_color, "diamonds_color_prop.csv",
+#           row.names = FALSE)
+
+unique(df$color)
+length(df$color)
+unique(length(df$color))
+
+df_agg = aggregate(data = df, price ~ cut, FUN = "mean")
+df_agg
+
+# write.csv(df_agg, "diamonds_price_cut_mean.csv",
+#           row.names = FALSE)
