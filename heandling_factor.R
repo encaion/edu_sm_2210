d1d2f33222c27c9f2ebf69180e9199a458d4832d
Seung Wook Kim <CEO@Rloha.io>
2019-05-10 16:42:48 +0900

    Create heandling_factor.R

---
+++ heandling_factor.R
@@ -0,0 +1,14 @@
+fac = factor(c(2, 4, 6, 8, 8, 8))
+fac
+
+fac[3]
+fac[5] = 20
+fac
+
+fac = factor(c(2, 4, 6, 8, 8))
+fac
+
+num = as.numeric(fac)
+as.numeric(as.character(fac))
+# aws = read.delim("AWS_sample.txt", sep = "#",
+#                  stringsAsFactors = FALSE)
